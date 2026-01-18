bf241164b3b01b7a792b557cf2182073b2e870f0
Carlos Bernardes <contato@carloshb.com.br>
2026-01-18 17:57:00 -0300

    chore(deps): atualiza Storybook para v10.1.11 e dependências
    - Atualiza @storybook/react, @storybook/react-vite e storybook para ^10.1.11
    - Adiciona @storybook/addon-docs e eslint-plugin-storybook
    - Atualiza yarn.lock com novas dependências
    - Ajusta .eslintrc.cjs para compatibilidade com Storybook v10
    
    Esta atualização resolve incompatibilidades de peer dependencies
    com Vite 7.2.4, já que Storybook 10 suporta oficialmente Vite >= 5.0.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,13 +5,7 @@ module.exports = {
     es2020: true,
     node: true,
   },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react/recommended',
-    'plugin:react-hooks/recommended',
-    'prettier',
-  ],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react/recommended', 'plugin:react-hooks/recommended', 'prettier', 'plugin:storybook/recommended'],
   ignorePatterns: ['dist', '.eslintrc.cjs', 'node_modules', 'coverage'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
